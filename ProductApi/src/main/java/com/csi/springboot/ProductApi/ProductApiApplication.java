92ae095fe04bfcbd72267a93c2df7ca072590afa
Ujwal <Ujwal@LAPTOP-M15JH3GI>
2019-12-18 10:14:26 +0530

    Syso added

---
+++ ProductApi/src/main/java/com/csi/springboot/ProductApi/ProductApiApplication.java
@@ -0,0 +1,14 @@
+package com.csi.springboot.ProductApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductApiApplication.class, args);
+		System.out.println("\n this is GitDemo");
+	}
+
+}
