5eb2b03bdccf28acaf708a1b45c84d250922a13d
Ujwal <Ujwal@LAPTOP-M15JH3GI>
2019-12-18 11:00:52 +0530

    syso added

---
+++ ProductPersistence/src/main/java/com/csi/springboot/ProductPersistence/ProductPersistenceApplication.java
@@ -0,0 +1,13 @@
+package com.csi.springboot.ProductPersistence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductPersistenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductPersistenceApplication.class, args);
+	}
+
+}
---
+++ ProductPersistence/src/test/java/com/csi/springboot/ProductPersistence/ProductPersistenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csi.springboot.ProductPersistence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductPersistenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
